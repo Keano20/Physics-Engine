b836e6530042fc5572d6d62ead0267d5033f19a4
Keano Ricketts <keano.ricketts@yahoo.com>
2025-01-31 12:30:35 +0000

    Create the application loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::RenderWindow window (sf::VideoMode(720, 500), "Physics Playground");
+    window.setFramerateLimit(10);
+
+    // Application loop
+    while (window.isOpen()) {
+
+        // Handles window events
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Sets the window background to grey
+        window.clear(sf::Color(100, 100, 100));
+
+        // Display the content
+        window.display();
+    }
+
     return 0;
 }
