7d723552574514b7c89c3748f37b9fbdeab0d127
Keano Ricketts <keano.ricketts@yahoo.com>
2025-03-14 19:56:22 +0000

    Create particle class that inherits from the RigidBody class.

---
+++ include/physics/Particle.h
@@ -0,0 +1,20 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
+
+#include"../../include/physics/RigidBody.h" // Include our RigidBody class for inheritence
+
+class Particle : public RigidBody {
+  private:
+    float lifespan;
+
+    public:
+      Particle(sf::Vector2f startPosition, float mass, float lifespan);
+
+      void update(float deltaTime); //Allow overiding of the update method
+      void draw(sf::RenderWindow& window);
+      bool isActive() const; // Check if the particle is still active
+};
+
+
+
+#endif //PARTICLE_H
