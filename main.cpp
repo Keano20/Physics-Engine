3add050b557823e2ebf315d731bf6599770ed5b0
Keano Ricketts <keano.ricketts@yahoo.com>
2025-03-27 18:17:58 +0000

    Switch to TestPhysicsWorld for isolated test environment - Enabled TestPhysicsWorld::runTestEnviroment() for testing purposes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "include/core/Engine.h"
+#include "tests/TestPhysicsWorld.h"
 
 int main() {
     Engine engine;
     engine.run();
+
+    //TestPhysicsWorld testPhysicsWorld;
+    //testPhysicsWorld.runTestEnvironment();
 
     return 0;
 }
