afd6c2f378e9826fc758befbc997c308342a4489
Keano Ricketts <keano.ricketts@yahoo.com>
2025-04-02 15:43:06 +0100

    Updated test environment and CMakeLists.txt: - Fixed SFML 3.0 event handling in TestPhysicsWorld.cpp. - Refactored video mode and event loop to work with SFML 3.0 API. - Updated CMakeLists.txt to use version-agnostic SFML paths for macOS and Windows.

--- tests/TestPhysicsWorld.cpp
+++ tests/TestPhysicsWorld.cpp
@@ -7,17 +7,17 @@
 #include <SFML/Graphics.hpp>
 
 void TestPhysicsWorld::runTestEnvironment() {
-    sf::RenderWindow window(sf::VideoMode(720, 500), "TESTING ENVIRONMENT");
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(720, 500)), "TESTING ENVIRONMENT");
     sf::Clock clock;
 
     std::unique_ptr<Particle> particle = std::make_unique<Particle>(sf::Vector2f(100.f, 100.f), 1.f, 5.f);
 
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()){
                 window.close();
         }
+    }
 
         float deltaTime = clock.restart().asSeconds();
         particle->update(deltaTime);
