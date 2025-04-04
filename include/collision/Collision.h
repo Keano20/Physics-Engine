edef8af154562c485604780acbd7d825f3f01295
Keano Ricketts <keano.ricketts@yahoo.com>
2025-04-04 19:42:49 +0100

    Add public methods for particle and border collisions

--- include/collision/Collision.h
+++ include/collision/Collision.h
@@ -1,9 +1,12 @@
 #ifndef PHYSICS_ENGINE_COLLISION_H
 #define PHYSICS_ENGINE_COLLISION_H
-
+#include <SFML/Graphics.hpp>
+#include "../../include/physics/Particle.h"
 
 class Collision {
-
+public:
+    static void resolveParticleCollision(Particle& particleA, Particle& ParticleB);
+    static void resolveBorderCollision(Particle& particle, const sf::RenderWindow& window);
 };
 
 
