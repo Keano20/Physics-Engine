a17d36b6e1a5bf5e9cc68b96b5f8dba0c11bfd43
Keano Ricketts <keano.ricketts@yahoo.com>
2025-04-04 20:21:09 +0100

    Spawn multiple particles and apply gravity each frame - Replaced single particle with loop to spawn 10 particles at startup - Applied gravity force to all particles during each update cycle - Refactored event loop to use SFML 3's std::optional event polling syntax

--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -3,7 +3,11 @@
 
 Engine::Engine() :window(sf::VideoMode(sf::Vector2u(720, 500)), "Physics Playground") {
     window.setFramerateLimit(60); // sets the frame rate
-    particles.push_back(std::make_unique<Particle>(sf::Vector2f(100.f, 100.f), 1.f, 15.f));
+    //particles.push_back(std::make_unique<Particle>(sf::Vector2f(100.f, 100.f), 1.f, 15.f)); //Spawns a single particle
+    for (int i = 0; i < 10; i++) {
+        sf::Vector2f particlePosition(360.f + i * 10.f, 250.f);
+        particles.push_back(std::make_unique<Particle>(particlePosition, 1.5, 15));
+    }
 }
 
 void Engine::run() {
@@ -15,7 +19,7 @@ void Engine::run() {
 }
 
 void Engine::processEvents() {
-    while (const std::optional event = window.pollEvent()) {
+    while (const std::optional<sf::Event> event = window.pollEvent()) {
         if (event->is<sf::Event::Closed>()) {
             window.close();
         }
@@ -28,6 +32,10 @@ void Engine::update() {
         if (particle-> isActive()) {
             particle->update(deltaTime);
         }
+    }
+    for (auto& particle : particles) {
+        particle->applyForce(gravity.getForce());
+        particle->update(deltaTime);
     }
 }
 
